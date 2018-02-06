0b8b6e0804f8029131acd13ff183573be52e36e1
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-02-06 13:31:44 +0100

    Update ansible-hosts.sh

--- ubuntu/ansible/ansible-hosts.sh
+++ ubuntu/ansible/ansible-hosts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-chmod og-wx ~/.ssh/authorized_keys
+su vagrant -c "cat /vagrant/id_rsa.pub >> ~/.ssh/authorized_keys"
+su vagrant -c "chmod og-wx ~/.ssh/authorized_keys"
