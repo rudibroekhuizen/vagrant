48231e11af72ad3e7e5c2db397519c18e198701c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-02-06 15:25:17 +0100

    Update ansible-control.sh

--- ubuntu/ansible/ansible-control.sh
+++ ubuntu/ansible/ansible-control.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Install Ansible
 sudo apt-get update
-#sudo apt-get -y upgrade
 sudo apt-get -y install software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install ansible
 
+# Set up keys
 su vagrant -c "ssh-keygen -t rsa -f /vagrant/id_rsa -N ''"
 su vagrant -c "mv /vagrant/id_rsa ~/.ssh/"
+
+# Add hosts to hosts file
+sed -i 's/#host_key_checking/host_key_checking/g' /etc/ansible/ansible.cfg
+
+# Disable ssh host verification
+sed -i 's/#host_key_checking/host_key_checking/g' /etc/ansible/ansible.cfg
