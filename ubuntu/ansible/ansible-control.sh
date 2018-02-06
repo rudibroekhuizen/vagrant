aecf1c6077aa2ce9a5f1a127aae167ef374ea129
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-02-06 12:23:53 +0100

    Update ansible-control.sh

--- ubuntu/ansible/ansible-control.sh
+++ ubuntu/ansible/ansible-control.sh
@@ -6,3 +6,5 @@ sudo apt-get -y install software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install ansible
+
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
