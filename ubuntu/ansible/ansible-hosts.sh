0206c2fa0cd176e39ac66c368bc1b4b9f8d883d7
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-02-08 15:17:36 +0100

    Update ansible-hosts.sh

--- ubuntu/ansible/ansible-hosts.sh
+++ ubuntu/ansible/ansible-hosts.sh
@@ -8,6 +8,6 @@ mkdir -p /home/$ANSIBLE_USER/.ssh
 echo "$ANSIBLE_USER ALL=NOPASSWD:ALL" > /etc/sudoers.d/$ANSIBLE_USER
 
 # Manage public key
-cat /vagrant/id_rsa.pub >> $ANSIBLE_USER/.ssh/authorized_keys
+cat /vagrant/id_rsa.pub >> /home/$ANSIBLE_USER/.ssh/authorized_keys
 chown -R $ANSIBLE_USER:$ANSIBLE_USER /home/$ANSIBLE_USER/.ssh
 chmod 600 /home/$ANSIBLE_USER/.ssh/authorized_keys
