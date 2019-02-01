cc1b1bde95896adbd8e35d58a9a4edac507733a7
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2019-02-01 15:28:52 +0100

    Reorganise

---
+++ ubuntu-ansible/README.md
@@ -0,0 +1,1 @@
+Vagrant with Ansible provider
---
+++ ubuntu-puppet/desktop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install build-essential
+sudo apt-get -y install ubuntu-desktop
